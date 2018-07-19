ffd4a8c1c1cff6bc0f285056a7c719af21aa812b
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-19 18:16:41 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,15 @@
 def prime?(number)
   is_prime = true
 
-
+if number > 1
   for i in 2..number - 1
-    if number < 1
-      is_prime = false
     elsif number % i == 0
       is_prime = false
     end
   end
+else
+  is_prime = false
+end
 
   if is_prime
     return true
