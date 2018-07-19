2b2213f0e2457893e0c7d514de171ee16deff59e
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-19 17:52:34 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,5 @@
 def prime?(number)
-  if number > 1
-    (2..(number)).each do |i|
-      if number % i == 0
-        return true
-      else
-        return false
-      end
-    end
-    true
-  else
-    false
-  end
+  if number == 1 
+    return false 
+  end 
 end
