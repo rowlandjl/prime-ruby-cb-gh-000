e2428cca19693ea4869f79fc96e0a6a1397cedcd
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-19 18:17:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(number)
+  is_prime = true
+
+if number > 1
+  for i in 2..number - 1
+    if number % i == 0
+      is_prime = false
+    end
+  end
+else
+  is_prime = false
+end
+
+  if is_prime
+    return true
+  else
+    return false
+  end
+
+end
